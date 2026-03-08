9b0828b10c68551f26c55c36ce53e93393d37464
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-03-08 13:18:30 +0000

    update

--- array/prog7.c
+++ array/prog7.c
@@ -37,3 +37,34 @@
 //     printf("\n");
 // }
 #include<stdio.h>
+int main()
+{
+    int size,temp;
+    scanf("%d",&size);
+       int arr[size];
+       for (int i=0;i<size;i++)
+       {
+        scanf("%d",&arr[i]);
+
+       }
+       for(int k=0;k<size;k++)
+       {
+        for(int j=0;j<size;j++)
+        {
+           if (arr[k]<arr[j])
+           {
+            temp=arr[k];
+            arr[k]=arr[j];
+            arr[j]=temp;
+           }
+        }
+         
+
+        
+       }
+       for (int i=0;i<size;i++)
+       {
+        printf("%d",arr[i]);
+       }
+
+}
