e3a57f6421490b25a04150e149ef2f12d1eac9f8
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-03-12 13:01:40 +0000

    update

--- stdio/1.c
+++ stdio/1.c
@@ -73,24 +73,66 @@
 // return 0;
 // }#include<stdio.h>
 //-----------------------------------------HOW THE ferror and the clearerror functions--------------------------
-#include<stdio.h>
+// #include<stdio.h>
+// int main()
+// {
+//     FILE*fp;
+//     fp=fopen("new.txt","w");
+//     if(fp==NULL)
+//     {
+//         perror("error");
+//         return -1;
+
+//     }
+//     char ch;
+//     ch=fgetc(fp);
+//     if(ferror(fp))
+//     fprintf(stderr,"ERROR IN reading ");
+//     clearerr(fp);
+//     if((ferror(fp)))
+//     printf("error in reasing ");
+//     fclose (fp);
+//     return 0;
+// }
+//-------------------------------------------ftell---------------------------------------
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+//   FILE *fptr;
+// char ch;
+// fptr = fopen("1.c", "r");
+// /* Need to do error checking on fopen() */
+// printf("File offset is at : %ld\n\n", ftell(fptr));
+
+// while ((ch = fgetc(fptr)) != EOF)
+// fputc(ch, stdout);
+// printf("\nat the end file offset is at :%ld\n", ftell(fptr));
+// fclose(fptr);
+// return 0;
+// }
+//-----------------------------------------------------------wap to program 
+ #include <stdio.h>
 int main()
 {
-    FILE*fp;
-    fp=fopen("new.txt","w");
-    if(fp==NULL)
-    {
-        perror("error");
-        return -1;
+    int num1, num2;
+    float num3;
+    char str[10], oper, ch;
+    FILE*fptr;
+    if ((fptr = fopen("text.txt", "w+")) == NULL)
+  {
+          fprintf(stderr, "Can't open input file text.txt!\n");
+              return 1;
+   }
 
-    }
-    char ch;
-    ch=fgetc(fp);
-    if(ferror(fp))
-    fprintf(stderr,"ERROR IN reading ");
-    clearerr(fp);
-    if((ferror(fp)))
-    printf("error in reasing ");
-    fclose (fp);
-    return 0;
+   fprintf(fptr, "%d %c %d %s %f\n", 2, '+', 1, "is", 1.1);
+   //rewind(fptr);
+fscanf(fptr, "%d %c %d %s %f", &num1, &oper, &num2, str, &num3);
+fclose(fptr);
+fopen(fptr);
+
+printf("%d %c %d %s %f\n", num1, oper, num2, str, num3);
+
+fclose(fptr);
+return 0;
 }
