f0e37ede91e4aa29fe6916f8f226d56d786afee6
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-03-15 11:47:39 +0000

    update

--- assignment/3a.c
+++ assignment/3a.c
@@ -1,14 +1,98 @@
-#include<stdio.h>
-int main ()
+// #include<stdio.h>
+// int main ()
+// {
+//     int a=0,i,b=1,c=0;
+//     printf("enter the number");
+//     scanf("%d0",);
+//     while( i<=num1;i++)
+//     {
+//         printf("%d\n",a);
+//         c=a+b;
+//         c=b;
+//         b=a;
+//     }
+// }
+#include <stdio.h>
+
+char *my_strtok(char *str, const char *delim)
 {
-    int a=0,i,b=1,c=0;
-    printf("enter the number");
-    scanf("%d0",);
-    while( i<=num1;i++)
+    static char *ptr;
+    int i;
+
+    if (str != NULL)
+        ptr = str;
+
+    if (ptr == NULL)
+        return NULL;
+
+    //Skip delimiters 
+    while (*ptr)
     {
-        printf("%d\n",a);
-        c=a+b;
-        c=b;
-        b=a;
+        for (i = 0; delim[i]; i++)
+        {
+            if (*ptr == delim[i])
+                break;
+        }
+
+        if (delim[i] == '\0')
+            break;
+
+        ptr++;
     }
+
+    if (*ptr == '\0')
+        return NULL;
+
+    char *start = ptr;
+
+    /* Find next delimiter */
+    while (*ptr)
+    {
+        for (i = 0; delim[i]; i++)
+        {
+            if (*ptr == delim[i])
+            {
+                *ptr = '\n';   // replace delimiter with newline
+                ptr++;
+                return start;
+            }
+        }
+        ptr++;
+    }
+
+    return start;
+}
+
+int main()
+{
+    char str1[100];
+    char str2[50];
+    char *token;
+
+    printf("Enter string1 : ");
+    scanf("%[^\n]", str1);
+
+    getchar();
+
+    printf("Enter string2 : ");
+    scanf("%[^\n]", str2);
+
+    token = my_strtok(str1, str2);
+
+    printf("Tokens :\n");
+
+    while (token != NULL)
+    {
+        /* Print token until newline */
+        while (*token && *token != '\n')
+        {
+            printf("%c", *token);
+            token++;
+        }
+        printf("\n");
+
+        token = my_strtok(NULL, str2);
+    }
+
+    return 0;
 }
--- stdio2/class.c
+++ stdio2/class.c
@@ -23,14 +23,67 @@
 // puts("EOF Received");
 // return 0;
 // }
+// #include<stdio.h>
+// #include<unistd.h>
+// int main()
+// {
+//     while(1)
+//     {
+//         putc('A',stdout);
+//         sleep(1);
+//     }
+// }
+// //move the newline character
+
+
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
+
 int main()
 {
+    int width=30,height=10;
+    int ballx=10,bally=3;
+    int dx=1,dy=1;
+    int paddlex=12;
+    char ch;
+
     while(1)
     {
-        putc('A',stdout);
-        sleep(1);
+        system("clear");   // use "cls" in windows
+
+        for(int y=0;y<height;y++)
+        {
+            for(int x=0;x<width;x++)
+            {
+                if(y==bally && x==ballx)
+                    printf("O");
+                else if(y==height-1 && x>=paddlex && x<paddlex+5)
+                    printf("=");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+
+        ballx+=dx;
+        bally+=dy;
+
+        if(ballx<=0 || ballx>=width-1)
+            dx=-dx;
+
+        if(bally<=0)
+            dy=-dy;
+
+        if(bally==height-2 && ballx>=paddlex && ballx<paddlex+5)
+            dy=-dy;
+
+        usleep(100000);
+
+        printf("Move paddle (a/d): ");
+        scanf(" %c",&ch);
+
+        if(ch=='a') paddlex--;
+        if(ch=='d') paddlex++;
     }
 }
-//move the newline character
