192d8dd46630d16f1cd3a758da6b3140d6b51e61
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-03-17 04:44:04 +0000

    update

--- file/1.c
+++ file/1.c
@@ -1,6 +1,22 @@
+//--------------------------------this is where the normmal file read without the error broii----------
+// #include<stdio.h>
+// int main()
+// {
+//     FILE* ptr=fopen("new.txt","r");
+//     fclose(ptr);
+// }
+//=======================================the read but but the not wxisting file show the segmentation fault thats why we are using the 
+// validation for that to over come error brrooo 
 #include<stdio.h>
 int main()
 {
-    FILE* ptr=fopen("new.txt","r");
-    fclose(ptr);
+    FILE* ptr=fopen("new.tt","r");
+    if(ptr==NULL)
+    {
+        printf("hu mav atta kelsa");
+        return -1;
+    }
+    fclose(ptr);// if the file not exist the we have return the in validation only beacuse at last in 
+    // fclose the it closes the NULL it means the segnetation fault maam did u understand
+    
 }
