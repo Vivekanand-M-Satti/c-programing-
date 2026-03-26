8d8b2b35fd9c3f0ba140fd38a36777d27dd6dd3d
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-03-26 14:08:46 +0000

    update

--- strgclass/1.c
+++ strgclass/1.c
@@ -1,3 +1,4 @@
+// example 06 in the ppt 
 #include<stdio.h>
 int main()
 {
@@ -6,5 +7,7 @@ int main()
         int j=0;
         printf("%d",i);
     }
-    printf("%d",j);
+    printf("%d",j);//ANSWER IS - shows the error:undeclared 
+    //--becausee it delclred in on another in and out  it is auto variable so thats why the life time of that is only in between
+    // the start and the exit so it is the exited after} okay boosss clear ?
 }
