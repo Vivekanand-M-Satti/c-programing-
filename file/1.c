5a6d0913feb72d35fd37d9ec8ae13a94b68d8722
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-03-31 03:50:04 +0000

    update

--- file/1.c
+++ file/1.c
@@ -7,19 +7,18 @@
 // }
 //=======================================the read but but the not wxisting file show the segmentation fault thats why we are using the 
 // validation for that to over come error brrooo 
-// #include<stdio.h>
-// int main()
-// {
-//     FILE* ptr=fopen("new.tt","r");
-//     if(ptr==NULL)
-//     {
-//         printf("hu mav atta kelsa");
-//         return -1;
-//     }
-//     fclose(ptr);// if the file not exist the we have return the in validation only beacuse at last in 
-    // fclose the it closes the NULL it means the segnetation fault maam did u understand
-
-//}
+#include<stdio.h>
+int main()
+{
+    FILE* ptr=fopen("new.tt","r");
+    if(ptr==NULL)
+    {
+        printf("hu mav atta kelsa");
+        return -1;
+    }
+    fclose(ptr);// if the file not exist the we have return the in validation only beacuse at last in 
+    //fclose the it closes the NULL it means the segnetation fault maam did u understand
+}
 //----------------------------------read and the print the file by using thee while loop========
 // #include<stdio.h>
 // int main()
@@ -134,14 +133,3 @@
 
 //     fclose(fp);
 // }
-#include<stdio.h>
-int main()
-{
-    while(1)
-    {
-
-    
-    printf("hello");
-    sleep(1);
-    }
-}
