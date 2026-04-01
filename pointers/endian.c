e6c441c26a2b5003d4333c537d82a3e50458811e
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-01 13:02:34 +0000

    update

--- pointers/endian.c
+++ pointers/endian.c
@@ -50,9 +50,56 @@
 // }
 
 
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+// Function to swap characters
+void swap(char *a, char *b)
+{
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Recursive function
+void combination(char *str, int l, int r)
+{
+    if (l == r)
+    {
+        printf("%s\n", str);
+        return;
+    }
+
+    for (int i = l; i <= r; i++)
+    {
+        swap(&str[l], &str[i]);           // Fix one character
+        combination(str, l + 1, r);       // Recurse
+        swap(&str[l], &str[i]);           // Backtrack
+    }
+}
+
 int main()
 {
-    int *num;
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Check duplicate characters
+    for (int i = 0; str[i]; i++)
+    {
+        for (int j = i + 1; str[j]; j++)
+        {
+            if (str[i] == str[j])
+            {
+                printf("Error: please enter distinct characters\n");
+                return 0;
+            }
+        }
+    }
+
+    int n = strlen(str);
+    combination(str, 0, n - 1);
+
     return 0;
 }
