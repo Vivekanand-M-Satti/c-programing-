0d514b686b68f2c10a5c0853989d986d0a0d5b16
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-03 10:32:10 +0000

    update

---
+++ pointers/nullptr.c
@@ -0,0 +1,13 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int*ptr=NULL;
+//     printf("%d",*ptr);//Segmentation fault (core dumped)
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int*ptr=NULL;
+//     ptr=123;
+//     printf("%d",*ptr);//Segmentation fault (core dumped)
+// }
