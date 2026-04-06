70076f642c543bec05fc65383dc79f1270282b11
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-06 15:33:56 +0000

    update

--- pointers/Voidptr.c
+++ pointers/Voidptr.c
@@ -1,6 +1,28 @@
+// #include<stdio.h>
+// int main()
+// {
+//     void *ptr;
+//     printf("%zu",sizeof(*ptr));
+// }
+// swap the numbers by using the void pointer 
 #include<stdio.h>
+void swap(void *f,void*s,int size)
+{
+
+}
 int main()
 {
-    void *ptr;
-    printf("%zu",sizeof(*ptr));
+    int n1 =10,n2=20;
+    printf("Bef: %d %d\n",n1,n2);
+    swap(&n1,&n2,sizeof(int));
+
+    char c1 ='A',c2='Z';
+    printf("Bed :%c %c\n",c1,c2);
+    swap(&c1,&c2,sizeof(char));
+
+
+
+    
+     
+
 }
