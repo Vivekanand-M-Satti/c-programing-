2610b2bc9f0345bca1a699226e420a2c61330cab
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-07 10:28:02 +0000

    update

---
+++ dynamic/malloc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char *ptr;
+    ptr=malloc(6);
+    if(ptr==NULL)
+    {
+        perror("Error");
+        return -1;
+    }
+    strcpy(ptr,"vivek");
+    puts(ptr);
+    printf("%s",ptr);
+    free(ptr);
+    return 0;
+}
