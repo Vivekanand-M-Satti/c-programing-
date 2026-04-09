faa90324d4a7ed494e26cddde954bd4430e73851
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-09 17:49:37 +0000

    update

--- bitwise/class1.c
+++ bitwise/class1.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int main()
 {
-   unsigned char a=0x67,b=0x39;
-    printf("%x\n",a&b);
-     printf("%x\n",a|b);
-      printf("%x\n",a^b);
-       printf("%hhx\n",~a);
-       
+   // unsigned char a=0x67,b=0x39;
+   //  printf("%x\n",a&b);
+   //   printf("%x\n",a|b);
+   //    printf("%x\n",a^b);
+   //     printf("%hhx\n",~a);
+   char str[5];
+fgets(str, sizeof(str), stdin);
+
+printf("%s",str);
+
 
 }
