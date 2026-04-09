003f3bc05b024a268c5a037628696d89c7f503a9
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-09 07:34:57 +0000

    update

--- dynamic/constptr.c
+++ dynamic/constptr.c
@@ -8,16 +8,28 @@
 // printf("Number is %d\n", num);
 // return 0;
 // }
-#include <stdio.h>
+// example ----------------------------------2
+// #include <stdio.h>
+// int main()
+// {
+// int num = 100;
+// const int *iptr = &num;
+// printf("Number is %d\n", num);
+// num = 200;
+
+// printf("Number is %d\n", *iptr);
+// // *iptr = 899;
+// printf("Number is %d\n", *iptr);
+// return 0;
+// }
+// -----------------------------------example -3
+#include<stdio.h>
 int main()
 {
-int num = 100;
-const int *iptr = &num;
-printf("Number is %d\n", num);
-num = 200;
-
-printf("Number is %d\n", *iptr);
-// *iptr = 899;
-printf("Number is %d\n", *iptr);
-return 0;
+    int x=10,y=20;
+    int *const ptr=&x;
+    printf("*ptr : %d\n",*ptr);
+    ptr=&y;// compiler error 
+    printf("*ptr : %d \n",*ptr);
+    
 }
