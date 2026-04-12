fe99847263725dbfdc7904143cb3b8358d870821
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-12 08:55:58 +0000

    update

---
+++ CLA2.0/1.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+
+// int main(int argc, char *argv[]) {
+//     printf("Total arguments: %d\n", argc);
+
+//     for(int i = 0; i < argc; i++) {
+//         printf("argv[%d] = %s\n", i, argv[i]);
+//     }
+
+//     return 0;
+// }
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+      int num = atoi(argv[1]);
+    printf("Integer: %d\n", num);
+    float num1 = atof(argv[2]);
+    printf("Float: %f\n", num1);
+    return 0;
+}
