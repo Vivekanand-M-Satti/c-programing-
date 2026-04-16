2a79e52a0545bcde83c1b6ddd26bf1f1cf69167d
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-16 12:57:17 +0000

    update

--- CLA2.0/1.c
+++ CLA2.0/1.c
@@ -1,20 +1,36 @@
+// // #include <stdio.h>
+
+// // int main(int argc, char *argv[]) {
+// //     printf("Total arguments: %d\n", argc);
+
+// //     for(int i = 0; i < argc; i++) {
+// //         printf("argv[%d] = %s\n", i, argv[i]);
+// //     }
+
+// //     return 0;
 // #include <stdio.h>
+// #include <stdlib.h>
 
 // int main(int argc, char *argv[]) {
-//     printf("Total arguments: %d\n", argc);
-
-//     for(int i = 0; i < argc; i++) {
-//         printf("argv[%d] = %s\n", i, argv[i]);
-//     }
-
+//       int num = atoi(argv[1]);
+//     printf("Integer: %d\n", num);
+//     float num1 = atof(argv[2]);
+//     printf("Float: %f\n", num1);
 //     return 0;
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-      int num = atoi(argv[1]);
-    printf("Integer: %d\n", num);
-    float num1 = atof(argv[2]);
-    printf("Float: %f\n", num1);
-    return 0;
+// }
+#include<stdio.h>
+#include<stdlib.h>
+int main(int a,char *ar[])
+{
+    if(a==1)
+    {
+        return 1;
+    }
+    float sum=0;
+    for(int i=0;i<a;i++)
+    {
+        sum+=atoi(ar[i]);
+    }
+    printf("Average %f",sum/(a-1));
+    
 }
