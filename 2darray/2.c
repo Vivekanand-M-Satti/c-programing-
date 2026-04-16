d03c6837d2c67499e94fb334b63a4dde9238eb30
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-16 10:54:58 +0000

    update

--- 2darray/2.c
+++ 2darray/2.c
@@ -1,25 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int sum=0,min;
-    //scanf("%d%d",&a,&b);
-    int arr[2][3]={10,20,30,40,50,60};
-    sum=arr[0][0];
+    int sum=0,min,a,b,s1=0,s2=0,s3=0;
+    scanf("%d%d",&a,&b);
+    int arr[a][b];//={10,20,30,40,50,60};
+    sum=-3333;
     min=arr[0][0];
-    for (int i=0;i<2;i++)
+    for (int i=0;i<a;i++)
     {
-        for (int j=0;j<3;j++)
+        for (int j=0;j<b;j++)
         {
-            if(arr[i][j]>sum)
-            {
-                sum=arr[i][j];
-            }
-            if(arr[i][j]<min)
-            {
-                min=arr[i][j];
-            }
+            scanf("%d",&arr[i][j]);
+            s1=0;
+            
         }
     }
-    printf(" the largest is %d\n",sum);
-    printf("the smallest is %d",min);
+    for (int i=0;i<a;i++)
+    {
+        for (int j=0;j<b;j++)
+        {
+            // 
+            if(i==s2)
+            {
+                s1+=arr[i][j];
+
+            }
+            s2++;
+             printf("%d\n",s1);
+        }
+    }
+    //printf(" the largest is %d\n",sum);
+    //printf("the smallest is %d",min);
+    printf("%d\n",s1);
+    printf("%d",s2);
 }
