984cd4f9f79b576edc0d430b5fc24529b276ae11
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-25 17:02:30 +0000

    update

--- rough/1.c
+++ rough/1.c
@@ -1,60 +1,72 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-int main()
-{
-    int a ;
-    int n[a=10];
+// int main()
+// {
+//     int a ;
+//     int n[a=10];
 
-    printf("Array size = %d", a);
-    return 0;
-}
+//     printf("Array size = %d", a);
+//     return 0;
+// }
 
-////ternary 
-#include<stdio.h>
-int main()
-{
-    char n;
-    printf("enter the number");
-    scanf("%c",&n);
-    (n>='A'&&n<='Z')?printf("lower case %c",(n+32)):
-    (n>='a'&&n<='z')?printf("upper case %c",(n-32)):// why we cannot use the'32' in addition
-    printf("%c",n);
-}
+// ////ternary 
+// #include<stdio.h>
+// int main()
+// {
+//     char n;
+//     printf("enter the number");
+//     scanf("%c",&n);
+//     (n>='A'&&n<='Z')?printf("lower case %c",(n+32)):
+//     (n>='a'&&n<='z')?printf("upper case %c",(n-32)):// why we cannot use the'32' in addition
+//     printf("%c",n);
+// }
 
-#include<stdio.h>
-int main()
-{
-    char n;
-    printf("enter the number:");
-    scanf(" %c",&n);
-    (n>='A'&&n<='Z')?printf("Upper case")://$ also digit
-    (n>='a'&&n<='z')?printf("lower case"):
-    (n>='0'&&n<='9')?printf("digit"):
-    printf("special character");
+// #include<stdio.h>
+// int main()
+// {
+//     char n;
+//     printf("enter the number:");
+//     scanf(" %c",&n);
+//     (n>='A'&&n<='Z')?printf("Upper case")://$ also digit
+//     (n>='a'&&n<='z')?printf("lower case"):
+//     (n>='0'&&n<='9')?printf("digit"):
+//     printf("special character");
 
-}
-//-----------------------------------------check the size and get the out by using the 2 variable ...................20/02/26
-#include<stdio.h>
-int  print(int arr[],int size)
-{
-       if (size==0)
-       {
-      return 0;
-       }
+// }
+// //-----------------------------------------check the size and get the out by using the 2 variable ...................20/02/26
+// #include<stdio.h>
+// int  print(int arr[],int size)
+// {
+//        if (size==0)
+//        {
+//       return 0;
+//        }
        
     
 
-           int j= print(arr,size-1);
-            printf("%d ",arr[j]);
+//            int j= print(arr,size-1);
+//             printf("%d ",arr[j]);
 
-    }
-int main()
-{
-    int arr[5]={5,6,7,8,9};
-    int size=5;
-    print(arr,size);
+//     }
+// int main()
+// {
+//     int arr[5]={5,6,7,8,9};
+//     int size=5;
+//     print(arr,size);
 
-}
+// }
 //------------------------------------HW the reverse the array by using the pointer and the refcursion-----------
 // -----------------------------------which is the largest digit in the  the pointer and the refcursion------------
 //----------------------palinfrome or not ---------------------------------
+#include<stdio.h>
+void change_bit()
+int main()
+{
+    unsigned int a;
+    scanf("%X",&a);
+    unsigned char *aa=&a;
+    for(int i=0;i<4;i++)
+    {
+        printf("%x",aa[i]);
+    }
+}
