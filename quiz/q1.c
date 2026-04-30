21eaef136089b404325b9951cddf166c913d904e
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-04-30 11:12:06 +0000

    update

---
+++ quiz/q1.c
@@ -0,0 +1,26 @@
+ #if 0
+#include<stdio.h>
+int main()
+{
+    float x;
+    x=(3/2)(float);
+    printf("%f",x);
+
+}
+    #if 0
+    #endif
+    #include<stdio.h>
+    int main()
+    {
+        int a=(4,6,9,(68,89));
+        printf("%d",a);
+    }
+        #endif
+        
+    #if 1
+    #include<stdio.h>
+    int main()
+    {
+        printf("%zu",sizeof('a'));
+    }
+    #endif
