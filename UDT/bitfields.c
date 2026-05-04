8518e90f5fefb8be6c99ede340bf4daffe4bfdd6
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-04 13:51:38 +0000

    update

--- UDT/bitfields.c
+++ UDT/bitfields.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 struct Nibble
 {
-unsigned char lower : 4;
-unsigned char upper : 4;
+unsigned int lower : 33;
+unsigned int upper : 10;
 };
 int main()
 {
 struct Nibble nibble;
-nibble.upper = 0x0A;
-nibble.lower = 0x02;
-write
+// nibble.upper = 0x0A;
+// nibble.lower = 0x02;
 printf("%zu",sizeof(struct Nibble));
 return 0;
 }
