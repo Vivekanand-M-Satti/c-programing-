2286be30769a45d339e0c071ce931ca1b53fabf0
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-12 09:44:17 +0000

    update

---
+++ rough/perfectnum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num,sum=0;
+    scanf(  "%d",&num  );
+    for(int i=1;i<=num/2;i++)
+    {
+        if(num%i==0)
+        {
+            sum+=i;
+        }
+    }
+    if(sum==num)
+    {
+        printf("yes mav");
+
+    }
+    else{
+        printf("no mav");
+    }
+}
