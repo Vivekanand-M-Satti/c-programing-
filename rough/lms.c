50fdbafe7543d608b0cad3019aeebf04c9b9953e
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-15 08:30:46 +0000

    update

--- rough/lms.c
+++ rough/lms.c
@@ -1,58 +1,78 @@
-// // // 
-// // #include<stdio.h>
+// // // // 
+// // // #include<stdio.h>
 
-// //   print(int number)
-// // {
-// //     printf("%d", number);
-// //     return 89;
-// // }
+// // //   print(int number)
+// // // {
+// // //     printf("%d", number);
+// // //     return 89;
+// // // }
+
+// // // void main()
+// // // {
+// // //     int i = 999999;
+
+// // //     int k=print(i++);
+// // //      printf("%d", k);
+// // // }
+// // What is the output of this C code?
 
 // // void main()
-// // {
-// //     int i = 999999;
 
-// //     int k=print(i++);
-// //      printf("%d", k);
-// // }
-// What is the output of this C code?
+// // int i;
+
+// // int num_array[l;
+
+// // int *array_ptr = num_array;
+
+// // for (i = 0; i < 5; i++)
+
+// // printf("%d", *(array_ptr + i));
+
+// // What is the output of this C code?
+
+// // void main()
+
+// // int i;
+
+// // int num_array[l;
+
+// // int *array_ptr = num_array;
+
+// // for (i = 0; i < 5; i++)
+
+// // printf("%d", *(array_ptr + i));
+
+
+// #include <stdio.h>
 
 // void main()
+// {
+//     int i;
 
-// int i;
+//     int num_array[] = { /* no initialization */ };
 
-// int num_array[l;
+//     int *array_ptr = num_array;
 
-// int *array_ptr = num_array;
-
-// for (i = 0; i < 5; i++)
-
-// printf("%d", *(array_ptr + i));
-
-// What is the output of this C code?
-
-// void main()
-
-// int i;
-
-// int num_array[l;
-
-// int *array_ptr = num_array;
-
-// for (i = 0; i < 5; i++)
-
-// printf("%d", *(array_ptr + i));
-
-
+//     for (i = 0; i < 5; i++)
+//         printf("%d ", num_array[i]);
+// }
 #include <stdio.h>
 
-void main()
+int reverse(int no)
 {
-    int i;
+    if(no == 0)
+        return 0;
+    else
+        printf("%d, ", no);
 
-    int num_array[] = { /* no initialization */ };
+    reverse(no-1);
+}
 
-    int *array_ptr = num_array;
+int main()
+{
+    int no = 5;
 
-    for (i = 0; i < 5; i++)
-        printf("%d ", num_array[i]);
+    reverse(no);
+
+    return 0;
 }
