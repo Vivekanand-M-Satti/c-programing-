b97dcac733e69581f66b3a3c2813d8fdadfdcac8
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-16 12:06:17 +0000

    update

--- pointerstrings.c/1.c
+++ pointerstrings.c/1.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    char *s= "hello";
-    s[1]='i';
-    printf("%s",s);
+    // char *s= "hello";
+    // s[1]='i';
+    // printf("%s",s);
+    char str[10]="hello";
+     printf("%s",str);
 }
